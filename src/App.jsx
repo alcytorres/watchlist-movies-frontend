33211e49cbbdf8bc4e1a8919e9dbbc7e1259ee7e
Alcy Torres <mxalcytorres@gmail.com>
2024-07-15 12:53:02 -0400

    Frontend react authentication (signup, login, logout)

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+import { Signup } from "./Signup"
+import { Login } from "./Login";
+import { LogoutLink } from "./LogoutLink";
 import { Header } from "./Header";
 import { Content } from "./Content";
 import { Footer } from "./Footer";
@@ -5,6 +8,9 @@ import { Footer } from "./Footer";
 function App() {
   return (
     <div>
+      <Signup />
+      <Login />
+      <LogoutLink />
       <Header />
       <Content />
       <Footer />
