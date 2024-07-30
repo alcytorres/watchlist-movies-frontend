9b7bf966e7a0f8a92c2102c6af25bab1dd9bf34b
Alcy Torres <mxalcytorres@gmail.com>
2024-07-29 21:41:33 -0400

    Adjust card size + change background color + edit footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,11 @@ import { Header } from "./Header";
 import { Content } from "./Content";
 import { Footer } from "./Footer";
 import { BrowserRouter } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
-    <div>
+    <div className="app-container">
       <BrowserRouter>
         <Header />
         <Content />
--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,6 +1,7 @@
 export function Footer() {
   return (
     <footer>
+      <br></br>
       <p>Copyright 2024</p>
     </footer>
   )
