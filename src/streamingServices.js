782098b780e239fbf57e844b2628d9edc8294ab2
Alcy Torres <mxalcytorres@gmail.com>
2024-09-21 20:33:42 -0400

    Add streaming services icons + remove search from header

---
+++ src/streamingServices.js
@@ -0,0 +1,39 @@
+const streamingServices = [
+  {
+    id: 'max',
+    name: 'Max',
+    icon: '/icons/max.svg'
+  },
+  {
+    id: 'netflix',
+    name: 'Netflix',
+    icon: '/icons/netflix.svg'
+  },
+  {
+    id: 'amazon',
+    name: 'Amazon Prime Video',
+    icon: '/icons/amazon.svg'
+  },
+  {
+    id: 'disney',
+    name: 'Disney+',
+    icon: '/icons/disney.svg'
+  },
+  {
+    id: 'apple',
+    name: 'Apple TV+',
+    icon: '/icons/apple.svg'
+  },
+  {
+    id: 'paramount',
+    name: 'Paramount+',
+    icon: '/icons/paramount.svg'
+  },
+  {
+    id: 'hulu',
+    name: 'Hulu',
+    icon: '/icons/hulu.svg'
+  }
+];
+
+export default streamingServices;
